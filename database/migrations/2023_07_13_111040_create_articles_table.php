ec58c38ba8be5e18862ee320a8d6b59b221ec764
farhanakhanswe <farhanakhanswe@gmail.com>
2023-07-13 17:26:35 +0600

    migrations for articles, tags, categories and article_tag pivot table

---
+++ database/migrations/2023_07_13_111040_create_articles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            
+            $table->string('title')->unique();
+            $table->string('slug')->unique();
+            $table->text('summary');
+            $table->text('description');
+            $table->boolean('status')->boolean(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
