e6fec68e7c39d32d6abf7c79871134aa514a0283
farhanakhanswe <farhanakhanswe@gmail.com>
2023-07-14 12:48:57 +0600

    factories and seeders setup for dummy data insertion

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Tag;
+use App\Models\User;
+use App\Models\Article;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call(CategorySeeder::class);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->count(10)->create();
+
+        Article::factory()
+        ->has(Tag::factory()->count(2)) // each article has two tags
+        ->count(50)
+        ->create();
+        
     }
 }
