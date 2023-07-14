95fc07872faf9ea468f51ac50eafa752ac8db979
farhanakhanswe <farhanakhanswe@gmail.com>
2023-07-14 12:48:17 +0600

    datatype changed for description col in categories tb

--- database/migrations/2023_07_13_111029_create_categories_table.php
+++ database/migrations/2023_07_13_111029_create_categories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
-            $table->string('description')->nullable();
+            $table->longText('description')->nullable();
             $table->string('slug')->unique();
             $table->timestamps();
         });
